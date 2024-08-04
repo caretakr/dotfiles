0139f38f80562e3326a9e476ae5581f6d8d982e7
Caio A. da Silva <caretakr@proton.me>
2024-08-04 13:14:18 -0300

    Fix: XCursor environment

--- .scripts/hypr/initrc.d/10-cursor.sh
+++ .scripts/hypr/initrc.d/10-cursor.sh
@@ -7,5 +7,7 @@
 export HYPRCURSOR_THEME="rose-pine-hyprcursor"
 export HYPRCURSOR_SIZE="32"
 
+export XCURSOR_PATH="/usr/share/icons:${XDG_DATA_HOME}/icons"
+
 export XCURSOR_THEME="rose-pine-cursor"
 export XCURSOR_SIZE="32"
