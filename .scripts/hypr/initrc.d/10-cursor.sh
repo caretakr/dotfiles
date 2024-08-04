55075172746067190c2172685762de6b46701143
Caio A. da Silva <caretakr@proton.me>
2024-08-04 13:45:07 -0300

    Fix: GTK settings and cursor theme

--- .scripts/hypr/initrc.d/10-cursor.sh
+++ .scripts/hypr/initrc.d/10-cursor.sh
@@ -9,5 +9,5 @@ export HYPRCURSOR_SIZE="32"
 
 export XCURSOR_PATH="/usr/share/icons:${XDG_DATA_HOME}/icons"
 
-export XCURSOR_THEME="rose-pine-cursor"
+export XCURSOR_THEME="BreezeX-RoséPine"
 export XCURSOR_SIZE="32"
